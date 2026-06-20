62ab486359f9adf80e147c65cf0c78970434b450
MURUGANANDHAM <muruganandhamm7639@gmail.com>
2026-06-20 17:12:36 +0530

    last

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://code-hunt-qwqf.onrender.com/api',
+  baseURL: 'https://code-hunt-aiml.vercel.app',
   headers: { 'Content-Type': 'application/json' },
 });
 
